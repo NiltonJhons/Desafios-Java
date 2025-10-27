23c7331dce0b4293bf28b3aa4df8fefa1c36267c
Nilton Jhonas <niltonnjhonas@gmail.com>
2025-10-27 13:09:14 -0300

    Remove pasta out/production/src

--- src/src/Desafios/Nivel_3/Teste.java
+++
@@ -1,9 +0,0 @@
-package Desafios.Nivel_3;
-
-public class Teste {
-    static void main() {
-        String texto = "123aaBC";
-
-        System.out.println(!texto.matches(".*[a-zA-Z].*"));
-    }
-}
